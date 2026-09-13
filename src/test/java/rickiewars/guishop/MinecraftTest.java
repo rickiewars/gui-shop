0d86b06b66d84ddb4afd84beeba86a388593c377
rickiewars <29739029+rickiewars@users.noreply.github.com>
2026-09-13 20:20:34 +0200

    Fix stonecutter guard

--- src/test/java/rickiewars/guishop/MinecraftTest.java
+++ src/test/java/rickiewars/guishop/MinecraftTest.java
@@ -1,15 +1,14 @@
 package rickiewars.guishop;
 
 import net.minecraft.SharedConstants;
+//? if >=26.1 {
 import net.minecraft.core.component.DataComponentInitializers;
 import net.minecraft.core.registries.BuiltInRegistries;
+//?}
 import net.minecraft.server.Bootstrap;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import rickiewars.guishop.api.minecraft.impl.MinecraftCompat;
-
-//? if >=26.1 {
-//?}
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class MinecraftTest {
